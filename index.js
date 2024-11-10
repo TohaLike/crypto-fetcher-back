5332db2e58573331b280cde8f84ce3785277a098
Anton <antonpaper500@gmail.com>
2024-11-11 02:29:05 +0300

    added generator

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   origin: "https://crypto-fetcher-puce.vercel.app/"
 }));
 app.use("/api", routers)
