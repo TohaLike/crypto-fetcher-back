f2af441d721fe61e111354a823488e5ca25aef14
Anton <antonpaper500@gmail.com>
2024-11-11 02:21:17 +0300

    added generator

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const server = app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: "https://crypto-fetcher-puce.vercel.app",
     methods: ["GET", "POST"]
   },
   connectionStateRecovery: {
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: process.env.CLIENT_URL
+  origin: "https://crypto-fetcher-puce.vercel.app"
 }));
 app.use("/api", routers)
 app.use(errorMiddleware)
