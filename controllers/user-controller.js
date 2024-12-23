9d09ce8adbaf8ac545658532c40be0058fa2b294
Anton <antonpaper500@gmail.com>
2024-12-23 17:22:05 +0300

    updated upload profile image

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,7 +16,7 @@ class UserContoller {
 
       // secure: true, sameSite: 'none'
 
-      res.cookie("refreshToken", userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true, })
+      res.cookie("refreshToken", userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, })
       return res.json(userData)
     } catch (e) {
       next(e)
